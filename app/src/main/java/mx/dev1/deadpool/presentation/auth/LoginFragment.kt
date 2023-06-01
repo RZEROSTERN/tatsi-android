6d90d6d0316de101e7735a940ed758cf02f32bd7
Marco Ramirez <marco.ramirez@rzerocorp.com.mx>
2023-06-01 08:37:58 -0600

    Added form validation to login

--- app/src/main/java/mx/dev1/deadpool/presentation/auth/LoginFragment.kt
+++ app/src/main/java/mx/dev1/deadpool/presentation/auth/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import mx.dev1.deadpool.R
@@ -26,9 +27,49 @@ class LoginFragment : Fragment() {
     }
 
     private fun initView() {
+        binding.tatsiBtnLogin.setOnClickListener {
+            if(!validateLoginForm()) {
+                // Should do the login stuff
+                Toast.makeText(requireContext(),
+                    resources.getString(R.string.tatsi_test_form), Toast.LENGTH_LONG).show()
+            }
+        }
+
         binding.tatsiBtnRegister.setOnClickListener {
             findNavController().navigate(
                 R.id.action_loginFragment_to_registerFragment)
         }
+    }
+
+    private fun validateLoginForm(): Boolean {
+        var hasErrors = false
+
+        if(binding.tatsiTxtEmailLogin.text.toString().isBlank() ||
+                binding.tatsiTxtEmailLogin.text.toString().isNullOrEmpty()) {
+            binding.tatsiTxtlEmailLogin.error =
+                resources.getString(R.string.tatsi_email_empty_error)
+            hasErrors = true
+        } else {
+            binding.tatsiTxtlEmailLogin.error = null
+        }
+
+        if(binding.tatsiTxtPasswordLogin.text.toString().isBlank() ||
+                binding.tatsiTxtPasswordLogin.text.toString().isNullOrEmpty()) {
+            binding.tatsiTxtlPasswordLogin.error =
+                resources.getString(R.string.tatsi_password_empty_error)
+            hasErrors = true
+        }  else {
+            binding.tatsiTxtlPasswordLogin.error = null
+        }
+
+        if(binding.tatsiTxtPasswordLogin.text.toString().length < 6) {
+            binding.tatsiTxtlPasswordLogin.error =
+                resources.getString(R.string.tatsi_password_minlength_error)
+            hasErrors = true
+        } else {
+            binding.tatsiTxtlPasswordLogin.error = null
+        }
+
+        return hasErrors
     }
 }
--- app/src/main/java/mx/dev1/deadpool/presentation/auth/RegisterFragment.kt
+++ app/src/main/java/mx/dev1/deadpool/presentation/auth/RegisterFragment.kt
@@ -28,4 +28,8 @@ class RegisterFragment : Fragment() {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
     }
+
+    private fun validateRegisterForm(): Boolean {
+        return true
+    }
 }
