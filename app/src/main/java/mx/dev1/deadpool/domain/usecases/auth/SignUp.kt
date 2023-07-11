7d4c50482cd0a8daab5ec44105d8515710fe1d04
Marco Ramírez <marco.ramirez@rzerocorp.com>
2023-07-11 11:36:19 -0600

    Fix on FirebaseSignUp

--- app/src/main/java/mx/dev1/deadpool/domain/usecases/auth/SignUp.kt
+++ app/src/main/java/mx/dev1/deadpool/domain/usecases/auth/SignUp.kt
@@ -5,5 +5,6 @@ import mx.dev1.deadpool.domain.repositories.AuthRepository
 class SignUp(
     private val repo: AuthRepository
 ) {
-    suspend operator fun invoke(email: String, password: String) = repo.firebaseSignUp(email, password)
+    suspend operator fun invoke(email: String, password: String) =
+        repo.firebaseSignUp(email, password, firstName = "", lastName = "", phone = "")
 }
